7ce1b78f6311ffae1e55520be2a330ad326836dd
Bruno Roberto Búrigo <brunorb8@hotmail.com>
2016-05-10 18:29:53 -0300

    adding maxTags and rejectCallback options ; removing allowAdd option (redundant with the new maxTags: 0 option)

--- js/jquery.tagedit.js
+++ js/jquery.tagedit.js
@@ -1,53 +1,57 @@
 /*
-* Tagedit - jQuery Plugin
-* The Plugin can be used to edit tags from a database the easy way
-*
-* Examples and documentation at: tagedit.webwork-albrecht.de
-*
-* Copyright (c) 2010 Oliver Albrecht <info@webwork-albrecht.de>
-*
-* License:
-* This work is licensed under a MIT License
-* http://www.opensource.org/licenses/mit-license.php
-*
-* @author Oliver Albrecht Mial: info@webwork-albrecht.de Twitter: @webworka
-* Requires: jQuery v1.4+, jQueryUI v1.8+, jQuerry.autoGrowInput
-*
-* Example of usage:
-*
-* $( "input.tag" ).tagedit();
-*
-* Possible options:
-*
-*  deleteEmptyItems: true, // Deletes items with empty value
-*  additionalListClass: '', // put a classname here if the wrapper ul shoud receive a special class
-*  allowEdit: true, // Switch on/off edit entries
-*  allowAdd: true, // switch on/off the creation of new entries
-*  direction: 'ltr' // Sets the writing direction for Outputs and Inputs
-*  autocompleteOptions: {}, // Setting Options for the jquery UI Autocomplete (http://jqueryui.com/demos/autocomplete/)
-*  breakKeyCodes: [ 13, 44 ], // Sets the characters to break on to parse the tags (defaults: return, comma)
-*  texts: { // some texts
-*      removeLinkTitle: 'Remove from list.',
-*      saveEditLinkTitle: 'Save changes.',
-*      breakEditLinkTitle: 'Cancel'
-*  }
-*
-* Callbacks:
-*
-*  beforeAppend:
-*   triggered right before the append of a tag element. params: $li, element_id, value, basename
-*
-*  beforeSave:
-*      callback triggered when a new tag is being created, it receives the tag value as a parameter and should return
-*   a tag value as a result, if an empty string (or a coerced false value) is returned then the tag will not be created.
-*
-*  beforeRemove:
-*      Called right before a tag the removal of the <li> of a tag. params: $li
-*
-* Events:
-*
-*  transformToTag: triggered during the process of creation of a new tag (or when you select one from the autocomplete)
-*/
+ * Tagedit - jQuery Plugin
+ * The Plugin can be used to edit tags from a database the easy way
+ *
+ * Examples and documentation at: tagedit.webwork-albrecht.de
+ *
+ * Copyright (c) 2010 Oliver Albrecht <info@webwork-albrecht.de>
+ *
+ * License:
+ * This work is licensed under a MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author Oliver Albrecht Mial: info@webwork-albrecht.de Twitter: @webworka
+ * Requires: jQuery v1.4+, jQueryUI v1.8+, jQuerry.autoGrowInput
+ *
+ * Example of usage:
+ *
+ * $( "input.tag" ).tagedit();
+ *
+ * Possible options:
+ *
+ *  deleteEmptyItems: true, // Deletes items with empty value
+ *  additionalListClass: '', // put a classname here if the wrapper ul shoud receive a special class
+ *  allowEdit: true, // Switch on/off edit entries
+ *  direction: 'ltr' // Sets the writing direction for Outputs and Inputs
+ *  autocompleteOptions: {}, // Setting Options for the jquery UI Autocomplete (http://jqueryui.com/demos/autocomplete/)
+ *  breakKeyCodes: [ 13, 44 ], // Sets the characters to break on to parse the tags (defaults: return, comma)
+ *  texts: { // some texts
+ *      removeLinkTitle: 'Remove from list.',
+ *      saveEditLinkTitle: 'Save changes.',
+ *      breakEditLinkTitle: 'Cancel'
+ *  },
+ *  maxTags: integer // maximum number of allowed tags. Defaults to -1 (no limit for negative numbers)
+ *
+ * Callbacks:
+ *
+ *  beforeAppend:
+ *   triggered right before the append of a tag element. params: $li, element_id, value, basename
+ *
+ *  beforeSave:
+ *      callback triggered when a new tag is being created, it receives the tag value as a parameter and should return
+ *   a tag value as a result, if an empty string (or a coerced false value) is returned then the tag will not be created.
+ *
+ *  beforeRemove:
+ *      Called right before a tag the removal of the <li> of a tag. params: $li
+ *
+ *  rejectCallback:
+ *      Called when the inclusion of a tag is rejected by some rule (beforeSave, beforeAppend, maxTags, etc).
+ *       received params: event, a string with the name of the rule that prevented the inclusion of the tag
+ *
+ * Events:
+ *
+ *  transformToTag: triggered during the process of creation of a new tag (or when you select one from the autocomplete)
+ */
 
 (function($) {
 
@@ -57,14 +61,13 @@
 
 
 		/**
-		* Merge Options with defaults
-		*/
+		 * Merge Options with defaults
+		 */
 		options = $.extend(true, {
 			// default options here
-            checkToDeleteURL: null,
+			checkToDeleteURL: null,
 			additionalListClass: '',
 			allowEdit: true,
-			allowAdd: true,
 			direction: 'ltr',
 			autocompleteOptions: {
 				select: function(event, ui) {
@@ -85,9 +88,11 @@
 				deleteConfirmation: 'Are you sure to delete this entry?',
 				deletedElementTitle: 'This Element will be deleted.',
 				breakEditLinkTitle: 'Cancel',
-                forceDeleteConfirmation: 'There are more records using this tag, are you sure do you want to remove it?'
+				forceDeleteConfirmation: 'There are more records using this tag, are you sure do you want to remove it?'
 			},
 			tabindex: false,
+
+			maxTags: -1,
 
 			beforeSave: function(tag) {
 				return tag;
@@ -99,6 +104,10 @@
 
 			beforeAppend: function($li, elementId, value, baseName) {
 				return $li;
+			},
+
+			rejectCallback: function(event) {
+				// nothing
 			}
 		}, options || {});
 
@@ -183,108 +192,120 @@
 				.find('.tagedit-input')
 				.attr('tabindex', options.tabindex)
 				.each(function() { // Set function on the input
-				var that = this;
-				$(this).autoGrowInput({comfortZone: 15, minWidth: 15, maxWidth: 20000});
+					var that = this;
+					$(this).autoGrowInput({comfortZone: 15, minWidth: 15, maxWidth: 20000});
 
-				// Event is triggert in case of choosing an item from the autocomplete, or finish the input
-				$(this).on('transformToTag', function(event, id) {
-					var isFromDatabase = (typeof id != 'undefined' && (id.length > 0 || id > 0));
+					// Event is triggert in case of choosing an item from the autocomplete, or finish the input
+					$(this).on('transformToTag', function(event, id) {
+						var isFromDatabase = (typeof id != 'undefined' && (id.length > 0 || id > 0));
 
-					var checkAutocomplete = isFromDatabase == true? false : true;
-					// check if the Value ist new
-					var data = isNew($(this).val(), checkAutocomplete);
-					var isNewResult = data[0];
-					var foundId = data[1];
-					if(isNewResult === true || (isNewResult === false && foundId > 0)) {
+						var checkAutocomplete = isFromDatabase == true? false : true;
+						// check if the Value ist new
+						var data = isNew($(this).val(), checkAutocomplete);
+						var isNewResult = data[0];
+						var foundId = data[1];
+						if(isNewResult === true || (isNewResult === false && foundId > 0)) {
 
-						// happens when you typed a tag that already exists at the database
-						if(isFromDatabase == false && foundId > 0) {
-							isFromDatabase = true;
-							id = foundId;
-						}
-
-						if(options.allowAdd == true || isFromDatabase) {
-							var newTagValue = options.beforeSave(this.value);
-							if (newTagValue) {
-								var name = isFromDatabase ?
-									baseName + '[' + id + ']' : baseName + '[]';
-
-								// Make a new tag in front the input
-								var innHtml = '<li class="tagedit-listelement tagedit-listelement-old" ' +
-									'data-tagedit-fromdb="' + (isFromDatabase ? 'true' : 'false') + '" >';
-								innHtml += '<span dir="' + options.direction + '">' + newTagValue + '</span>';
-								innHtml += '<input type="hidden" name="' + name + '" value="' + newTagValue + '"/>';
-								innHtml += '<a class="tagedit-close" title="' + options.texts.removeLinkTitle + '">x</a>';
-								innHtml += '</li>';
-
-								var $newLi = options.beforeAppend($(innHtml), id, newTagValue, baseName);
-
-								$(this.parentNode).before($newLi);
+							// happens when you typed a tag that already exists at the database
+							if(isFromDatabase == false && foundId > 0) {
+								isFromDatabase = true;
+								id = foundId;
 							}
-						}
-					}
-					$(this).val('');
 
-					// close autocomplete
-					if(options.autocompleteOptions.source) {
-						$(this).autocomplete( "close" );
-					}
-
-				}).keydown(function(event) {
-					var code = event.keyCode > 0? event.keyCode : event.which;
-
-					switch(code) {
-						case 8: // BACKSPACE
-							if($(this).val().length == 0) {
-								// delete Last Tag
-								var $li = $tageditListUl.find('li.tagedit-listelement-old');
-								if (options.beforeRemove($li)) {
-									$tageditListUl.find('li.tagedit-listelement-old').last().remove();
+							if(options.maxTags !== 0 || isFromDatabase) {
+								if (options.maxTags > 0
+									&& $tageditListUl.find('li.tagedit-listelement-old').length >= options.maxTags
+								) { // reject append if maxTags is posive and we have reached the limit
+									options.rejectCallback('maxTags');
 								}
-								event.preventDefault();
-								return false;
+								else {
+									var value = this.value;
+									var newTagValue = options.beforeSave(value);
+									if (newTagValue) {
+										var name = isFromDatabase ?
+										baseName + '[' + id + ']' : baseName + '[]';
+
+										// Make a new tag in front the input
+										var innHtml = '<li class="tagedit-listelement tagedit-listelement-old" ' +
+											'data-tagedit-fromdb="' + (isFromDatabase ? 'true' : 'false') + '" >';
+										innHtml += '<span dir="' + options.direction + '">' + newTagValue + '</span>';
+										innHtml += '<input type="hidden" name="' + name + '" value="' + newTagValue + '"/>';
+										innHtml += '<a class="tagedit-close" title="' +
+											options.texts.removeLinkTitle + '">x</a>';
+										innHtml += '</li>';
+
+										var $newLi = options.beforeAppend($(innHtml), id, newTagValue, baseName);
+
+										$(this.parentNode).before($newLi);
+									}
+									else {
+										options.rejectCallback('beforeSave', value);
+									}
+								}
 							}
-							break;
-						case 9: // TAB
+						}
+						$(this).val('');
+
+						// close autocomplete
+						if(options.autocompleteOptions.source) {
+							$(this).autocomplete( "close" );
+						}
+
+					}).keydown(function(event) {
+						var code = event.keyCode > 0? event.keyCode : event.which;
+
+						switch(code) {
+							case 8: // BACKSPACE
+								if($(this).val().length == 0) {
+									// delete Last Tag
+									var $li = $tageditListUl.find('li.tagedit-listelement-old');
+									if (options.beforeRemove($li)) {
+										$tageditListUl.find('li.tagedit-listelement-old').last().remove();
+									}
+									event.preventDefault();
+									return false;
+								}
+								break;
+							case 9: // TAB
+								if($(this).val().length > 0 && $('ul.ui-autocomplete #ui-active-menuitem').length == 0) {
+									$(this).trigger('transformToTag');
+									event.preventDefault();
+									return false;
+								}
+								break;
+						}
+						return true;
+					}).keypress(function(event) {
+						var code = event.keyCode > 0? event.keyCode : event.which;
+						if($.inArray(code, options.breakKeyCodes) > -1) {
 							if($(this).val().length > 0 && $('ul.ui-autocomplete #ui-active-menuitem').length == 0) {
 								$(this).trigger('transformToTag');
-								event.preventDefault();
-								return false;
 							}
-						break;
-					}
-					return true;
-				}).keypress(function(event) {
-					var code = event.keyCode > 0? event.keyCode : event.which;
-					if($.inArray(code, options.breakKeyCodes) > -1) {
-						if($(this).val().length > 0 && $('ul.ui-autocomplete #ui-active-menuitem').length == 0) {
+							event.preventDefault();
+							return false;
+						}
+						return true;
+					}).on('paste', function(e){
+						var that = $(this);
+						if (e.type == 'paste'){
+							setTimeout(function(){
+								that.trigger('transformToTag');
+							}, 1);
+						}
+					}).on('blur', function() {
+						if(this.value.length == 0) {
+							// disable the field to prevent sending with the form
+							$(this).attr('disabled', 'disabled').addClass('tagedit-input-disabled');
+						}
+						else {
 							$(this).trigger('transformToTag');
 						}
-					event.preventDefault();
-					return false;
-					}
-					return true;
-				}).on('paste', function(e){
-					var that = $(this);
-					if (e.type == 'paste'){
-						setTimeout(function(){
-							that.trigger('transformToTag');
-						}, 1);
-					}
-				}).on('blur', function() {
-					if(this.value.length == 0) {
-						// disable the field to prevent sending with the form
-						$(this).attr('disabled', 'disabled').addClass('tagedit-input-disabled');
-					}
-					else {
-						$(this).trigger('transformToTag');
-					}
-				});
+					});
 
-				if(options.autocompleteOptions.source != false) {
-					$(this).autocomplete(options.autocompleteOptions);
-				}
-			}).end().click(function(event) {
+					if(options.autocompleteOptions.source != false) {
+						$(this).autocomplete(options.autocompleteOptions);
+					}
+				}).end().click(function(event) {
 				switch(event.target.tagName) {
 					case 'A':
 						var $li = $(event.target).parent();
@@ -297,7 +318,7 @@
 					case 'SPAN':
 					case 'LI':
 						if($(event.target).hasClass('tagedit-listelement-deleted') == false &&
-						$(event.target).parent('li').hasClass('tagedit-listelement-deleted') == false) {
+							$(event.target).parent('li').hasClass('tagedit-listelement-deleted') == false) {
 							// Don't edit an deleted Items
 							return doEdit(event);
 						}
@@ -312,11 +333,11 @@
 		})();
 
 		/**
-		* Sets all Actions and events for editing an Existing Tag.
-		*
-		* @param event {object} The original Event that was given
-		* return {boolean}
-		*/
+		 * Sets all Actions and events for editing an Existing Tag.
+		 *
+		 * @param event {object} The original Event that was given
+		 * return {boolean}
+		 */
 		function doEdit(event) {
 			if(options.allowEdit == false) {
 				// Do nothing
@@ -384,23 +405,23 @@
 					}
 					return true;
 				}).blur(function() {
-					var that = $(this);
-					closeTimer = window.setTimeout(function() {that.parent().trigger('finishEdit', [true])}, 500);
-				});
+				var that = $(this);
+				closeTimer = window.setTimeout(function() {that.parent().trigger('finishEdit', [true])}, 500);
+			});
 		}
 
 		/**
-		* Checks if a tag is already choosen.
-		*
-		* @param value {string}
-		* @param checkAutocomplete {boolean} optional Check also the autocomplet values
-		* @returns {Array} First item is a boolean, telling if the item should be put to the list, second is optional the ID from autocomplete list
-		*/
+		 * Checks if a tag is already choosen.
+		 *
+		 * @param value {string}
+		 * @param checkAutocomplete {boolean} optional Check also the autocomplet values
+		 * @returns {Array} First item is a boolean, telling if the item should be put to the list, second is optional the ID from autocomplete list
+		 */
 		function isNew(value, checkAutocomplete) {
-            checkAutocomplete = typeof checkAutocomplete == 'undefined'? false : checkAutocomplete;
+			checkAutocomplete = typeof checkAutocomplete == 'undefined'? false : checkAutocomplete;
 			var autoCompleteId = null;
 
-            var compareValue = value;
+			var compareValue = value;
 
 			var isNew = true;
 			$tageditListUl.find('li.tagedit-listelement-old input:hidden').each(function() {
@@ -414,12 +435,12 @@
 				if ($.isArray(options.autocompleteOptions.source)) {
 					result = options.autocompleteOptions.source;
 				}
-                else if ($.isFunction(options.autocompleteOptions.source)) {
+				else if ($.isFunction(options.autocompleteOptions.source)) {
 					options.autocompleteOptions.source({term: value}, function (data) {
 						result = data
 					});
 				}
-                else if (typeof options.autocompleteOptions.source === "string") {
+				else if (typeof options.autocompleteOptions.source === "string") {
 					var found = FOUND_TAGS[value];
 					if (found) {
 						result = [{value: found.value, id: found.id}];
@@ -448,7 +469,7 @@
 
 				// If there is an entry for that already in the autocomplete, don't use it
 				for (var i=0; i<result.length; i++) {
-                    var label = result[i].value;
+					var label = result[i].value;
 					if (label == compareValue) {
 						isNew = false;
 						autoCompleteId = result[i].id;
